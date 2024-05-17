0dd5c8ec929dcaf13f56c61a03617bb9d30a4811
Natasha Rodic <natashawolt@gmail.com>
2024-05-17 13:57:07 -0400

    Clean up the starter code

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -5,13 +5,13 @@ const User = require('../../models/user');
 module.exports = {
   create,
   login,
-  checkToken
+  // checkToken
 };
 
-function checkToken(req, res) {
-  console.log('req.user', req.user);
-  res.json(req.exp);
-}
+// function checkToken(req, res) {
+//   console.log('req.user', req.user);
+//   res.json(req.exp);
+// }
 
 async function create(req, res) {
   try {
@@ -26,7 +26,7 @@ async function create(req, res) {
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email});
+    const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) throw new Error();

325528c3e008c3cf3c8dc4c08d12dfca9c8038ac
Yi Chien Lee <yician.lee@gmail.com>
2024-05-20 21:42:03 -0700

    wip: revise the utility logic and controller for create course

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -4,12 +4,12 @@ const coursesCtrl = require('../../controllers/api/courses');
 const course = require('../../models/course');
 
 
-// GET /api/courses
-router.get('/', coursesCtrl.index);
-//POST/api/courses/new
-router.post('/', coursesCtrl.create);
 // GET /api/courses/:id
 router.get('/:id', coursesCtrl.show);
+// GET /api/courses
+router.get('/', coursesCtrl.index);
+// POST/api/courses/new
+router.post('/', coursesCtrl.create);
 
 
 module.exports = router;

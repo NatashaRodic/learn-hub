41a1c6c5979a6775a6b266394bc78fcdf512622f
Yi Chien Lee <yician.lee@gmail.com>
2024-05-21 10:12:43 -0700

    fix: routes conflicts

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -4,12 +4,14 @@ const coursesCtrl = require('../../controllers/api/courses');
 const course = require('../../models/course');
 
 
-// GET /api/courses/:id
-router.get('/:id', coursesCtrl.show);
 // GET /api/courses
 router.get('/', coursesCtrl.index);
+
 // POST/api/courses/new
 router.post('/', coursesCtrl.create);
+
+// GET /api/courses/:id
+router.get('/:id', coursesCtrl.show);
 
 
 module.exports = router;
